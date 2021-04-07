a30b6685e83f825a7f69757055f9a0db995e7c80
Katya Golovko <katyagolovko@coherentsolutions.com>
2021-04-07 18:25:08 +0300

    Initial commit for the fourth task.

--- consoleApp/src/main/java/com/issoft/store/StoreApp.java
+++ consoleApp/src/main/java/com/issoft/store/StoreApp.java
@@ -1,20 +1,54 @@
 package com.issoft.store;
 
+import java.io.*;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 
 public class StoreApp {
 
 
-    public static void main(String args[]) throws IllegalAccessException {
+    public static void main(String args[]) throws IllegalAccessException, IOException {
 
-        RandomStorePopulator randomStorePopulator = new RandomStorePopulator();
-        Store store = new Store();
-        store.setCategories(randomStorePopulator.populateProductLists());
-        List<Category> extractedData = randomStorePopulator.extractDataFromStore(store);
-        randomStorePopulator.pretty(extractedData);
+        int i = 1;
+        while(i>=1){
+            System.out.println("Hi! Enter your command: ");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            String userCommand = reader.readLine();
 
-    }
+            if(userCommand.equalsIgnoreCase("sort")){
+                RandomStorePopulator randomStorePopulator = new RandomStorePopulator();
+                Store store = new Store();
+                store.setCategories(randomStorePopulator.populateProductLists());
+                List<Category> extractedData = randomStorePopulator.extractDataFromStore(store);
+                Collections.sort(extractedData, (a, b) -> a.getName().compareToIgnoreCase(b.getName()));
+                randomStorePopulator.pretty(extractedData);
+            } else if(userCommand.equalsIgnoreCase("exit")) {
+                reader.close();
+                System.out.println("See you soon!");
+                i = 0;
+            }
+            else {
+                System.out.println("The command was not recognized. Available commands: sort, top, exit. Please try again.");
+            }
+        }
+
+
+        }
+
+
+
+
+
+
+
+
+        //Collections.sort(extractedData, (a, b) -> a.getName().compareToIgnoreCase(b.getName()));
+        //System.out.println(extractedData);
+
+
+
 
 
 }
